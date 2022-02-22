cb7dda1955d005b464b652f04ce7706b5490e943
Ruchan <jceadsa@gmail.com>
2022-02-22 20:55:02 +0300

    Debug banner removed

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData.dark(),
+      debugShowCheckedModeBanner: false,
       title: 'Json Test',
       home: HomeView(),
     );
